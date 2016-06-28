b71b8f605d54e17210542ab86ed9643dab048b5b
eli <elixircist@gmail.com>
2016-06-27 21:34:15 -0400

    formatted cli dbutil, added webserver

--- dbutil.py
+++ dbutil.py
@@ -2,42 +2,42 @@ import sqlite3
 
 class DBUtil:
     
-    #need to add mysql support
-    def __init__(self, Path):
-        self.conn = sqlite3.connect(Path)
-        self.c = self.conn.cursor()
-	self.conn.text_factory = str
-        self.path = Path
-        self.table = 'roster'
+	#need to add mysql support
+	def __init__(self, Path):
+		self.conn = sqlite3.connect(Path)
+		self.c = self.conn.cursor()
+		self.conn.text_factory = str
+		self.path = Path
+		self.table = 'roster'
 
-    #add column list?    
-    def insert(self, Name, Age, Club):
-        self.c.execute("INSERT INTO %s (name, age, club) VALUES ('%s', '%s', '%s');"
-                       % (self.table, Name, Age, Club))
-        self.conn.commit();
+	#add column list?    
+	def insert(self, Name, Age, Club):
+		self.c.execute("INSERT INTO %s (name, age, club) VALUES ('%s', '%s', '%s');"
+			% (self.table, Name, Age, Club))
+		self.conn.commit();
 
-    def query(self, columnList):
-        if type(columnList) != list:
-            print('columnList parameter needs to be a list')
-            return
+	def query(self, columnList):
+		if type(columnList) != list:
+			print('columnList parameter needs to be a list')
+			return
         
-        columnString = ''
-        for i in columnList:
-            columnString = columnString + i + ','
-        #take out the last comma
-        columnString = columnString[:-1]
+		columnString = ''
+		for i in columnList:
+			columnString = columnString + i + ','
+		#take out the last comma
+		columnString = columnString[:-1]
 	
-        self.conn.commit();
-
-        return self.c.execute('SELECT %s FROM %s;' % (columnString, self.table))
+		self.conn.commit();
+	
+		return self.c.execute('SELECT %s FROM %s;' % (columnString, self.table))
             
 
-    def update(self, setColumn, setValue, whereColumn, whereValue):
-        self.c.execute("UPDATE %s SET %s = '%s' WHERE %s = '%s';"
+	def update(self, setColumn, setValue, whereColumn, whereValue):
+		self.c.execute("UPDATE %s SET %s = '%s' WHERE %s = '%s';"
                        % (self.table, setColumn, setValue, whereColumn, whereValue))
 
-    def delete(self, column, value):
-	    self.c.execute("DELETE FROM %s WHERE %s = '%s';" % (self.table, column, value))
+	def delete(self, column, value):
+		self.c.execute("DELETE FROM %s WHERE %s = '%s';" % (self.table, column, value))
     
 
         
